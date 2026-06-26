3af0dc9b5ed434d84c79732b5bdb0531d2067da9
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-06-26 15:18:25 +0530

    page close button

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var allElems = document.querySelectorAll('.elem')
 var allFullElems = document.querySelectorAll('.fullElem')
+var closebtn = document.querySelectorAll('.fullElem .back')
 
 allElems.forEach(function (elem) {
     elem.addEventListener('click', function () {
         allFullElems[elem.id].style.display = 'block'
+    })
+})
+
+closebtn.forEach(function (back) {
+    back.addEventListener('click', function () {
+        allFullElems[back.id].style.display = 'none'
     })
 })

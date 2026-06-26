ef4b2a9efcf28e130dd6d6db6b15c0e6b927ea9d
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-06-26 13:49:01 +0530

    card section

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ var allElems = document.querySelectorAll('.elem')
 
 allElems.forEach(function (elem) {
     elem.addEventListener('click', function () {
-        // console.log(elem.id);
-
         console.log(document.querySelectorAll('.fullElem')[elem.id]);
     })
 })

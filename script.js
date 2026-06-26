59f992b27d9061d0bd75e10b9ac4168752fbedd5
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-06-26 13:58:06 +0530

    card section

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var allElems = document.querySelectorAll('.elem')
+var allFullElems = document.querySelectorAll('.fullElem')
 
 allElems.forEach(function (elem) {
     elem.addEventListener('click', function () {
-        console.log(document.querySelectorAll('.fullElem')[elem.id]);
+        allFullElems[elem.id].style.display = 'block'
     })
 })

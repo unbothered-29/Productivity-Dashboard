00a1f37c25e67c805571e329e4af260d95ebe4f3
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-06-26 15:30:42 +0530

    todo page

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-var allElems = document.querySelectorAll('.elem')
-var allFullElems = document.querySelectorAll('.fullElem')
-var closebtn = document.querySelectorAll('.fullElem .back')
+function openFeatures() {
+    var allElems = document.querySelectorAll('.elem')
+    var allFullElems = document.querySelectorAll('.fullElem')
+    var closebtn = document.querySelectorAll('.fullElem .back')
 
-allElems.forEach(function (elem) {
-    elem.addEventListener('click', function () {
-        allFullElems[elem.id].style.display = 'block'
+    allElems.forEach(function (elem) {
+        elem.addEventListener('click', function () {
+            allFullElems[elem.id].style.display = 'block'
+        })
     })
-})
 
-closebtn.forEach(function (back) {
-    back.addEventListener('click', function () {
-        allFullElems[back.id].style.display = 'none'
+    closebtn.forEach(function (back) {
+        back.addEventListener('click', function () {
+            allFullElems[back.id].style.display = 'none'
+        })
     })
-})
+}
+
+openFeatures()

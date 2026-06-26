bc75b6137da83ece28fc577f9e4fcda70d63519f
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-06-26 13:44:50 +0530

    hero section

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var allElems = document.querySelectorAll('.elem')
 
 allElems.forEach(function (elem) {
-    console.log(elem);
+    elem.addEventListener('click', function () {
+        // console.log(elem.id);
+
+        console.log(document.querySelectorAll('.fullElem'));
+    })
 })

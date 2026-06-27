174759fd58618789305435e49254f17fa1961688
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-06-27 15:45:03 +0530

    card section task input

--- script.js
+++ script.js
@@ -21,11 +21,52 @@ openFeatures()
 let form = document.querySelector('.addtask form')
 let taskInput = document.querySelector('.addtask form input')
 let taskDetailsInput = document.querySelector('.addtask form textarea')
+let taskCheckbox = document.querySelector('.addtask form #check')
+
+let currentTask = [
+    {
+        task: 'Do React',
+        details: 'Two-way binding',
+        imp: true
+    },
+    {
+        task: 'Do Java',
+        details: 'Loops & Patterns',
+        imp: true
+    },
+    {
+        task: ' Laxmi Bakes Meet',
+        details: 'Basic Set-up & workflow',
+        imp: false
+    },
+]
+
+function renderTask() {
+    var allTask = document.querySelector('.alltask');
+
+    var sum = ''
+
+    currentTask.forEach(function (elem) {
+        sum = sum + `   <div class="task">
+                        <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+                        <button>Mark as Completed</button>
+                        <!-- <button>Delete</button> -->
+                    </div>`
+    })
+
+    allTask.innerHTML = sum
+}
+
+renderTask()
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
 
-    console.log(taskInput.value);
-    console.log(taskDetailsInput.value);
+    // console.log(taskInput.value);
+    // console.log(taskDetailsInput.value);
+    // console.log(taskCheckbox.checked);
 
+    currentTask.push({ task: taskInput.value, details: taskDetailsInput.value, imp: taskCheckbox.checked })
+
+    console.log(currentTask);
 })

0a0a7892941c5d4352caae372fd57c4bc84de4ec
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-06-27 16:28:19 +0530

    task section

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ var markCompletedBtn = document.querySelectorAll('.task button')
 
 markCompletedBtn.forEach(function (btn) {
     btn.addEventListener('click', function () {
-        currentTask.splice(btn.id, 1)
+   
     })
 })
 

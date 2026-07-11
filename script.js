af8b00d2b90dae36817a7318c7149c1c149c7fa6
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-07-11 16:35:36 +0530

    project completed

--- script.js
+++ script.js
@@ -388,7 +388,6 @@ function dailyGoals() {
         else {
             // list.innerHTML = goals.map(function (g) {
             //     return `<div class="goal-item ${g.done ? 'done' : ''}" style="border-left-color: var(--${g.color})">
-            //         <input type="checkbox" data-id="${g.id}" ${g.done ? 'checked' : ''}>
            
         }
 
